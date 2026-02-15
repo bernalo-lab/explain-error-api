a1db5bb2279dc6f09ee8dd56bbaaff8e57181026
bernalo-lab <bsogeke@bernalo.com>
2026-02-15 14:52:56 +0000

    Update server.js
    Added two production-grade improvements v4

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(
       "http://127.0.0.1:5500"
     ],
     methods: ["GET", "POST", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"]
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
